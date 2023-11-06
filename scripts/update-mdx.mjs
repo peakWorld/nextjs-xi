7cac88dd38dcaa8f8d1d800cbca118cf1d8347c7
windlliu <windlliu@tencent.com>
2023-11-06 15:32:03 +0800

    feat: 测试blog

--- scripts/update-mdx.mjs
+++ scripts/update-mdx.mjs
@@ -56,8 +56,6 @@ function cryptPwd(text) {
   return md5.update(text).digest("hex");
 }
 
-// console.log("settings", stagedFiles, AFiles, DFiles);
-
 fs.writeJsonSync(settingFile, settings, { spaces: 2, encoding: "utf-8" });
 
 execSync(`git add ${settingFile}`);
